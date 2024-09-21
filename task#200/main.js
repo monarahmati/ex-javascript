b233b345b4a7d5bb1825fd5ba24043c401c64f93
monarahmati <monarahmati.official@gmail.com>
2024-09-21 11:46:25 +0330

    first commit

---
+++ task#200/main.js
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+
+
+// ________________________________________________________________________________
+
+// const Students = [ 'mona' , 'mina' , 'omid' , 'arizo' , 'ahmad' , 'reza' , 'nahid' , 'Negar']
+// const number = Math.floor(Students.length / 2);
+// console.log(number);
+
+// //slice a arry
+// const fristStudents = Students.slice(0 , number )
+// const secondStudents = Students.slice(number)
+// const newArraye = [ ...fristStudents , "NOSHIN" , ...secondStudents ]
+
+// //log
+// console.log( 'Students =>' , Students);
+// console.log('fristStudents =>' , fristStudents);
+// console.log('secondStudents =>' , secondStudents);
+// console.log('newArraye =>' , newArraye);
+
+// _______________________________________________________________________________
+
+
+// students.push('fati')//add new index
+
+
+// function to change items of arrey
+// const newList = ( students , oldIndex , newIndex ) =>{
+
+//     const element = students[oldIndex]
+//     students.splice( oldIndex , 1)
+//     students.splice(newIndex , 0 , element)
+
+// }
+
+// newList(students , 4 , 2)
+// console.log(students);
+
+// ______________________________________________________________________________
+
